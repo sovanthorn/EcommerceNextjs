1244d6e6fca3bb8588864cd0012383068dcdaa63
Sovanthorn <nounsovannthon@gmail.com>
2024-04-06 16:07:38 +0700

    add

---
+++ Components/css-nav-bar/menu.ts
@@ -0,0 +1,23 @@
+export const MenuList = [
+    {
+        name: "Home",
+        path: "/",
+        active: false,
+    },
+    {
+        name: "Products",
+        path: "/products",
+        active: true,
+    },
+    {
+        name: "About",
+        path: "/about",
+        active: false,
+    },
+    {
+        name: "Contact Us",
+        path: "/contact",
+        active: false,
+    },
+    
+]
---
+++ app/(admin)/dashboard/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Dashboard() {
+    return (
+      <div className='h-screen grid place-content-center text-6xl'>Dashboard</div>
+    )
+  }
